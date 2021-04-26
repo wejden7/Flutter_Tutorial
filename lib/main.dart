12ec468734010c955168090efa3b03b49167c292
wejden <wejdenchneti7@gmail.om>
2021-04-26 21:26:07 +0100

    container DecorationImage image NetworkImage

--- lib/main.dart
+++ lib/main.dart
@@ -8,27 +8,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Course Flutter" ,
-      home:Scaffold(
-        appBar: AppBar(),
-        drawer: Drawer(),
-        body:Container(
-          //decoration
-          decoration: BoxDecoration(
-              color:Colors.red,
-              border: Border.all(
-                color: Colors.green,
-                width: 10,
-              ), 
-              //borderRadius: BorderRadius.circular(100),
-              borderRadius: BorderRadius.only(bottomLeft:Radius.circular(100) ,bottomRight: Radius.circular(50))
-                    ),
-          child:Text(
-          "How are you",
-          style:TextStyle(fontSize: 30.0,) ,
-        ),
-        )
-      )
-    );
+        title: "Course Flutter",
+        home: Scaffold(
+            appBar: AppBar(),
+            drawer: Drawer(),
+            body: Container(
+              //decoration
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                          "https://th.bing.com/th/id/OIP.GHPQTvkT3TqCadzvWvCZFgHaGs?pid=ImgDet&rs=1"))),
+              child: Text(
+                "How are you",
+                style: TextStyle(
+                  fontSize: 30.0,
+                ),
+              ),
+            )));
   }
 }
