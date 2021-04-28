9cf7ded947f0a72b3dd4a5481a87c92a080dfe9e
wejden <wejdenchneti7@gmail.om>
2021-04-28 22:01:07 +0100

    new point

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -7,62 +7,7 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   @override
-  bool usa = false;
-  bool sy = false;
-  bool fr = false;
-
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Text(
-            "choose Country",
-            style: TextStyle(fontSize: 30),
-          ),
-          Row(
-            children: [
-              Text("USE"),
-              Checkbox(
-                  activeColor: Colors.red,
-                  checkColor: Colors.black,
-                  value: usa,
-                  onChanged: (val) {
-                    setState(() {
-                      usa = val;
-                      print("USA=$usa | val=$val");
-                    });
-                  })
-            ],
-          ),
-          Row(
-            children: [
-              Text("SY"),
-              Checkbox(
-                  value: sy,
-                  onChanged: (val) {
-                    setState(() {
-                      sy = val;
-                      print("SY=$sy | val=$val");
-                    });
-                  })
-            ],
-          ),
-          Row(
-            children: [
-              Text("FR"),
-              Checkbox(
-                  value: fr,
-                  onChanged: (val) {
-                    setState(() {
-                      fr = val;
-                      print("FR=$fr | val=$val");
-                    });
-                  })
-            ],
-          ),
-        ],
-      ),
-    );
+    return Container();
   }
 }
