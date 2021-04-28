62dabb4fd98b01dd3ec28e75484c87668a4dbfc6
wejden <wejdenchneti7@gmail.om>
2021-04-28 03:22:04 +0100

    Single Child Scroll View

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         title: "Course Flutter",
         home: Scaffold(
-          appBar: AppBar(),
-          drawer: Drawer(),
-          body: Container(),
-        ));
+            appBar: AppBar(),
+            drawer: Drawer(),
+            body: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    height: 100,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    width: 100,
+                    height: 100,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            )));
   }
 }
