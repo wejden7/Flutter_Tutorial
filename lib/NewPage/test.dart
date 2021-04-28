b172a52584eea4f5f5ba594dc7b7bed3e2508caf
wejden <wejdenchneti7@gmail.om>
2021-04-28 23:09:53 +0100

    radio

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Test extends StatefulWidget {
   @override
@@ -7,7 +8,73 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   @override
+  String country;
+  String prog;
   Widget build(BuildContext context) {
-    return Container();
+    return Wrap(
+      children: [
+        Row(children: [
+          Text("Usa"),
+          Radio(
+              activeColor: Colors.red,
+              value: "use",
+              groupValue: country,
+              onChanged: (val) {
+                setState(() {
+                  country = val;
+                });
+              }),
+          Text("FR"),
+          Radio(
+              value: "FR",
+              groupValue: country,
+              onChanged: (val) {
+                setState(() {
+                  country = val;
+                });
+              }),
+          Text("TN"),
+          Radio(
+              value: "TN",
+              groupValue: country,
+              onChanged: (val) {
+                setState(() {
+                  country = val;
+                });
+              }),
+        ]),
+        Row(
+          children: [
+            Text("Java"),
+            Radio(
+                value: "Java",
+                groupValue: prog,
+                onChanged: (val) {
+                  setState(() {
+                    prog = val;
+                  });
+                }),
+            Text("c++"),
+            Radio(
+                value: "c++",
+                groupValue: prog,
+                onChanged: (val) {
+                  setState(() {
+                    prog = val;
+                  });
+                }),
+            Text("Dart"),
+            Radio(
+                value: "Dart",
+                groupValue: prog,
+                onChanged: (val) {
+                  setState(() {
+                    prog = val;
+                  });
+                }),
+          ],
+        )
+      ],
+    );
   }
 }
