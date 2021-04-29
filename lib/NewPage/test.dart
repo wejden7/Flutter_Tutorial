6257515ff3fbdb1f3019cd47cb8601a34742fddb
wejden <wejdenchneti7@gmail.om>
2021-04-29 02:34:58 +0100

    Switch

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -6,8 +6,41 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
+  bool notif = false;
+  bool mot = false;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Wrap(
+        children: [
+          Text(
+            "On/Off",
+            style: TextStyle(fontSize: 20),
+          ),
+          Switch(
+              activeColor: Colors.red,
+              activeTrackColor: Colors.blue,
+              inactiveThumbColor: Colors.black,
+              inactiveTrackColor: Colors.green,
+              value: notif,
+              onChanged: (val) {
+                setState(() {
+                  notif = val;
+                });
+              }),
+          SwitchListTile(
+              title: Text("ON/OFF"),
+              controlAffinity: ListTileControlAffinity.leading,
+              subtitle: Text("required"),
+              secondary: Icon(Icons.call_received_sharp),
+              value: mot,
+              onChanged: (val) {
+                setState(() {
+                  mot = val;
+                });
+              })
+        ],
+      ),
+    );
   }
 }
