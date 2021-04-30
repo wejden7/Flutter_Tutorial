2cf88b350a1864c158e670742dc05ba7720fa051
wejden <wejdenchneti7@gmail.om>
2021-04-30 17:53:27 +0100

    TextFormField
    disabledBorder enabledBorder borderSide

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -14,18 +14,14 @@ class _TestState extends State<Test> {
         body: Container(
           padding: EdgeInsets.all(10),
           child: TextFormField(
-            scrollPadding: EdgeInsets.all(20),
-
-            keyboardType: TextInputType.multiline,
-            // initialValue: "input",
             decoration: InputDecoration(
-              hintText: "inpute",
-              labelText: "Flutter",
-              prefixIcon: Icon(Icons.ac_unit),
-              suffixIcon: Icon(Icons.ac_unit),
-              filled: true,
-              fillColor: Colors.red,
-            ),
+                //  enabled: false,
+                //  disabledBorder: UnderlineInputBorder(
+                //     borderSide: BorderSide(color: Colors.green, width: 3)),
+                enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.red, width: 3)),
+                focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 3))),
           ),
         ),
       ),
