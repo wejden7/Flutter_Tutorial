c48158389083fc153a472533fa8ab3fb9fcf6279
wejden <wejdenchneti7@gmail.om>
2021-04-30 17:42:23 +0100

    TextFormField

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -6,71 +6,27 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
-  PageController pc;
-  @override
-  void initState() {
-    pc = new PageController(
-        initialPage: 1, keepPage: true, viewportFraction: 0.75);
-    super.initState();
-  }
-
-  List images = [
-    {
-      "image":
-          "https://i.pinimg.com/originals/7e/9b/65/7e9b65b5d6f56313795ecf375ecd6480.jpg"
-    },
-    {
-      "image":
-          "https://i.pinimg.com/originals/7a/f9/b2/7af9b249e46e94eee81cdf7097f67202.jpg"
-    },
-    {"image": "https://i.redd.it/h8dzlb3w5sv41.jpg"},
-    {
-      "image":
-          "https://i.pinimg.com/originals/1b/c8/92/1bc892daa1f76a0219d3c0fd596ecb26.jpg"
-    },
-    {
-      "image":
-          "https://i.pinimg.com/originals/1b/c8/92/1bc892daa1f76a0219d3c0fd596ecb26.jpg"
-    },
-  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter"),
-          leading: Icon(Icons.ac_unit_outlined),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.access_alarms_rounded), onPressed: () {}),
-            IconButton(icon: Icon(Icons.face), onPressed: () {})
-          ],
-        ),
-        body: Column(
-          children: [
-            Container(
-              height: 300,
-              child: PageView.builder(
-                itemCount: images.length,
-                itemBuilder: (context, i) {
-                  return Image.network(images[i]["image"]);
-                },
-                controller: pc,
-                physics: BouncingScrollPhysics(),
-                onPageChanged: (index) {
-                  print(index);
-                },
-                reverse: true,
-                //scrollDirection: Axis.vertical,
-              ),
+        appBar: AppBar(),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: TextFormField(
+            scrollPadding: EdgeInsets.all(20),
+
+            keyboardType: TextInputType.multiline,
+            // initialValue: "input",
+            decoration: InputDecoration(
+              hintText: "inpute",
+              labelText: "Flutter",
+              prefixIcon: Icon(Icons.ac_unit),
+              suffixIcon: Icon(Icons.ac_unit),
+              filled: true,
+              fillColor: Colors.red,
             ),
-            ElevatedButton(
-                onPressed: () {
-                  pc.animateToPage(1,
-                      duration: Duration(seconds: 5), curve: Curves.bounceOut);
-                },
-                child: Text("go to page 2"))
-          ],
+          ),
         ),
       ),
     );
