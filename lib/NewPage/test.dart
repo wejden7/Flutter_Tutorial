2cb337f8d7483f3a96bf6324f5dbf2758bf1089c
wejden <wejdenchneti7@gmail.om>
2021-04-30 04:37:35 +0100

    TabBar And Tabs

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -21,168 +21,243 @@ class _TestState extends State<Test> with SingleTickerProviderStateMixin {
   TabController myControler;
   void initState() {
     print("Flutter a good FromWorek ");
-    myControler = new TabController(length: 3, vsync: this, initialIndex: 2);
+    myControler = new TabController(length: 3, vsync: this);
     super.initState();
   }
 
   Widget build(BuildContext context) {
-    return TabBarView(controller: myControler, children: [
-      ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
+    return MaterialApp(
+      title: "Course Flutter",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter"),
+          bottom: TabBar(
+            controller: myControler,
+            //isScrollable: true,
+            indicatorColor: Colors.white,
+            indicatorWeight: 5,
+            indicatorPadding: EdgeInsets.all(1),
+            labelColor: Colors.black,
+            onTap: (index) {
+              print(index);
+            },
+            tabs: [
+              Tab(child: Text("Flutter"), icon: Icon(Icons.one_k)),
+              Tab(
+                child: Icon(Icons.one_k),
+              ),
+              Tab(
+                child: Icon(Icons.one_k),
+              )
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.exit_to_app),
+              onPressed: () {},
+            ),
+            IconButton(icon: Icon(Icons.alarm), onPressed: () {})
+          ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                  currentAccountPicture: CircleAvatar(
+                    child: Text("W"),
+                    backgroundColor: Colors.red,
+                  ),
+                  accountName: Text("Wejden"),
+                  accountEmail: Text("Wejdenchneti7@gmail.com")),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("Home"),
+                selectedTileColor: Colors.red[100],
+                trailing: Icon(Icons.dock_sharp),
+                selected: true,
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("page1"),
+                trailing: Icon(Icons.dock_sharp),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("page2"),
+                trailing: Icon(Icons.dock_sharp),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text("page3"),
+                trailing: Icon(Icons.dock_sharp),
+              ),
+              ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text("Exite"),
+              ),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
+        ),
+        body: TabBarView(controller: myControler, children: [
+          ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+              ListTile(
+                leading: Icon(Icons.ondemand_video),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.grade),
+              ),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
+          ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+              ListTile(
+                leading: Icon(Icons.format_align_justify),
+                title: Text("Video"),
+                subtitle: Text("sous "),
+                trailing: Icon(Icons.face),
+              ),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-          ListTile(
-            leading: Icon(Icons.ondemand_video),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.grade),
-          ),
-        ],
+          ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.g_translate),
+                title: Text("Flutter"),
+                subtitle: Text("Dart "),
+                trailing: Icon(Icons.label),
+              ),
+            ],
+          )
+        ]),
       ),
-      ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-          ListTile(
-            leading: Icon(Icons.format_align_justify),
-            title: Text("Video"),
-            subtitle: Text("sous "),
-            trailing: Icon(Icons.face),
-          ),
-        ],
-      ),
-      ListView(
-        children: [
-          ListTile(
-            leading: Icon(Icons.g_translate),
-            title: Text("Flutter"),
-            subtitle: Text("Dart "),
-            trailing: Icon(Icons.label),
-          ),
-        ],
-      )
-    ]);
+    );
   }
 }
