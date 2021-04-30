dc9b7257da375e99f38856a2f6b05677c508c07c
wejden <wejdenchneti7@gmail.om>
2021-04-30 18:36:59 +0100

    validator

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -8,28 +8,51 @@ class Test extends StatefulWidget {
 
 class _TestState extends State<Test> {
   TextEditingController username = new TextEditingController();
+  GlobalKey<FormState> formState = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextFormField(
-                onTap: () {
-                  print("object");
-                },
-                onChanged: (string) {
-                  print("$string");
-                },
-                onEditingComplete: () {
-                  print("complite");
-                },
+        body: Form(
+          autovalidateMode: AutovalidateMode.always,
+          key: formState,
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  validator: (text) {
+                    if (text.length < 4) {
+                      return "false";
+                    }
+                    return null;
+                  },
+                ),
               ),
-            ),
-          ],
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  validator: (text) {
+                    if (text.length < 4) {
+                      return "false";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    var fomdata = formState.currentState;
+                    if (fomdata.validate()) {
+                      print("valider");
+                    } else {
+                      return null;
+                    }
+                  },
+                  child: Text("Send"))
+            ],
+          ),
         ),
       ),
     );
