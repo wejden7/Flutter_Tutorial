3ccbfe4c9ae15459f1c17e2c859dd99d9a3c883d
wejden <wejdenchneti7@gmail.om>
2021-04-30 18:13:40 +0100

    TextFormField
    controller

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Test extends StatefulWidget {
   @override
@@ -6,23 +7,34 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
+  TextEditingController username = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: TextFormField(
-            decoration: InputDecoration(
-                //  enabled: false,
-                //  disabledBorder: UnderlineInputBorder(
-                //     borderSide: BorderSide(color: Colors.green, width: 3)),
-                enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.red, width: 3)),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black, width: 3))),
-          ),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextFormField(
+                  cursorColor: Colors.red,
+                  cursorHeight: 30,
+                  cursorWidth: 20,
+                  //maxLength: 20,
+                  // maxLines: 2,
+                  //obscureText: true,
+                  textInputAction: TextInputAction.go,
+                  controller: username,
+                  decoration: InputDecoration()),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print(username.text);
+              },
+              child: Text("username.text"),
+            )
+          ],
         ),
       ),
     );
