dca481cd9f6514eb985b67a6a241b708b442af30
wejden <wejdenchneti7@gmail.om>
2021-05-02 02:36:38 +0100

    Slider

--- lib/NewPage/test.dart
+++ lib/NewPage/test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertuto/NewPage/one.dart';
-import 'package:fluttertuto/NewPage/tow.dart';
+import 'package:flutter/rendering.dart';
 
 class Test extends StatefulWidget {
   @override
@@ -8,10 +7,13 @@ class Test extends StatefulWidget {
 }
 
 class _TestState extends State<Test> {
+  var sliderValue = 10.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text("Flutter Home Page"),
+        ),
         body: Column(
           children: [
             ElevatedButton(
@@ -20,6 +22,18 @@ class _TestState extends State<Test> {
               },
               child: Text("Go To One Page"),
             ),
+            Slider(
+                max: 100,
+                min: 0,
+                activeColor: Colors.black,
+                inactiveColor: Colors.red,
+                label: "Wejden",
+                value: sliderValue,
+                onChanged: (x) {
+                  setState(() {
+                    sliderValue = x;
+                  });
+                })
           ],
         ));
   }
